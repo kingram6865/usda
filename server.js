4253ad56d20181f9e439b9bb942697d9fc0f497d
Ken Ingram <ken.ingram@gmail.com>
2023-10-31 19:41:26 -0700

    Flesh out details a bit.

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
+const color = require('./utilities/consoleColors')
 const express = require('express')
 const cors = require('cors')
 const logger = require('morgan')
-const db = require('./db/connection')
+// const db = require('./db/connection')
 const routes = require('./routes/main')
 const PORT = process.env.PORT || 3019
+const SERVER = process.env.HOST || localhost
 const TIME = new Date()
 // const SERVER = process.env.HOST || localhost
 console.log(`API Server:  ${process.env.HOST}:${process.env.PORT} on ${TIME}`)
@@ -11,9 +13,9 @@ console.log(`API Server:  ${process.env.HOST}:${process.env.PORT} on ${TIME}`)
 const app = express()
 app.use(cors())
 app.use(logger('dev'))
-// app.use(express.urlencoded({extended: true}))
 app.use(express.json())
-
+// app.use(express.urlencoded({extended: true}))
 app.use('/api', routes)
 
-app.listen(PORT, () => console.log(`Listening on ${PORT}`))
+let message = `Nutrition API Server Started -- Server: ${color.brightYellow}${SERVER}${color.Reset}, Port: ${color.brightYellow}${PORT}${color.Reset}, start time: (${color.brightGreen}${TIME.toLocaleString()}${color.Reset})`
+app.listen(PORT, () => console.log(message))
