d7673abfcb0821db6fe1683d325f2948e4822a09
Ken Ingram <ken.ingram@gmail.com>
2023-11-11 01:22:05 -0800

    Remove commented code.

--- server.js
+++ server.js
@@ -2,13 +2,10 @@ const color = require('./utilities/consoleColors')
 const express = require('express')
 const cors = require('cors')
 const logger = require('morgan')
-// const db = require('./db/connection')
 const routes = require('./routes/main')
 const PORT = process.env.PORT || 3019
 const SERVER = process.env.HOST || localhost
 const TIME = new Date()
-// const SERVER = process.env.HOST || localhost
-// console.log(`API Server:  ${process.env.HOST}:${process.env.PORT} on ${TIME}`)
 
 const app = express()
 app.use(cors())
@@ -16,11 +13,6 @@ app.use(cors())
 /* dev, combined or common */
 app.use(logger('common'))
 app.use(express.json())
-// app.use(express.urlencoded({extended: true}))
-
-
-
-
 app.use('/api', routes)
 
 let message = `Nutrition API Server Started -- Server: ${color.brightYellow}${SERVER}${color.Reset}, Port: ${color.brightYellow}${PORT}${color.Reset}, start time: (${color.brightGreen}${TIME.toLocaleString()}${color.Reset})`
