28147d30646ce4f9c478f066096567641c2e99fb
Ken Ingram <ken.ingram@gmail.com>
2023-11-19 21:34:58 -0800

    Add https server config.

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const color = require('./utilities/consoleColors')
+const fs = require('fs')
+const https = require('https')
 const express = require('express')
 const cors = require('cors')
 const logger = require('morgan')
@@ -12,8 +14,22 @@ app.use(cors())
 
 /* dev, combined or common */
 app.use(logger('common'))
+app.use(express.static(__dirname + '/static', { dotfiles: 'allow' }))
 app.use(express.json())
 app.use('/api', routes)
 
-let message = `Nutrition API Server Started -- Server: ${color.brightYellow}${SERVER}${color.Reset}, Port: ${color.brightYellow}${PORT}${color.Reset}, start time: (${color.brightGreen}${TIME.toLocaleString()}${color.Reset})`
+let httpMessage = `Nutrition API Server Started -- Server: ${color.brightYellow}${SERVER}${color.Reset}, Port: ${color.brightYellow}${PORT}${color.Reset}, start time: (${color.brightGreen}${TIME.toLocaleString()}${color.Reset})`
 app.listen(PORT, () => console.log(message))
+
+// https
+//   .createServer(
+//     {
+//       key: fs.readFileSync('/etc/letsencrypt/path/to/key.pem'),
+//       cert: fs.readFileSync('/etc/letsencrypt/path/to/cert.pem'),
+//       ca: fs.readFileSync('/etc/letsencrypt/path/to/chain.pem'),
+//     },
+//     app
+//   )
+//   .listen(443, () => {
+//     console.log('Listening...')
+//   })
